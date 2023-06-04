7aa78bf57ad81349334345eec1ce490a4baf36e8
pd212 <74095344+pd212@users.noreply.github.com>
2023-06-04 22:39:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
-
-
-
-
-
-
-
-//working code
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
