895aa3b38871186460eddf930d3072bd2772a6ab
pd212 <74095344+pd212@users.noreply.github.com>
2023-06-04 22:40:19 +0530

    Update postActions.js
    changes

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -18,19 +18,4 @@ export const fetchPosts = () => {
 
 
 
-// import { FETCH_POSTS } from "./type";
-
-// export const fetchPosts = () => async (dispatch) => {
-//   try {
-//     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-//     const posts = await response.json();
-
-//     dispatch({
-//       type: FETCH_POSTS,
-//       payload: posts,
-//     });
-//   } catch (error) {
-//     console.error('Error fetching posts:', error);
-//   }
-// };
 
